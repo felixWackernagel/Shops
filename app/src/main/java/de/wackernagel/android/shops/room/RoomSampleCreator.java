25f19bbab73fa12bad620aabd9c568548a711c7e
Felix Wackernagel <felix.wackernagel@gmail.com>
2018-02-06 13:01:24 +0100

    Add one sample brand

--- app/src/main/java/de/wackernagel/android/shops/room/RoomSampleCreator.java
+++ app/src/main/java/de/wackernagel/android/shops/room/RoomSampleCreator.java
@@ -7,6 +7,7 @@ public final class RoomSampleCreator {
     public static void createSamples( final AppDatabase db ) {
         brand( db, "Lidl Deutschland", "https://www.lidl.de", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Lidl-Logo.svg/200px-Lidl-Logo.svg.png" );
         brand( db, "ALDI Nord", "https://www.aldi-nord.de", "https://upload.wikimedia.org/wikipedia/commons/2/20/ALDI_Nord_Logo_2015.png" );
+        brand( db, "Konsum Dresden", "https://www.konsum.de", "https://upload.wikimedia.org/wikipedia/commons/e/e3/Konsum_logo_R.jpg" );
     }
 
     private static void brand( final AppDatabase database, final String name, final String websiteURL, final String imageURL ) {
